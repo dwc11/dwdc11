9612f9e96b4266a8224f344276f41bc674810405
dwc11 <dwc11@FSTU603030.pti.edu>
2019-05-02 18:12:29 -0400

    First Commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author dwc11
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    David();
+    
+    }
+  public static void David(){
+      System.out.println("David Cousins");
+}
+}
+;
